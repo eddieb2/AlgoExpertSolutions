95c9dbc617268a6c400b5b0d70583b65d37477a5
Edward Blanciak <blanciak.edward2@gmail.com>
2022-08-11 13:44:40 -0400

    ex16 and ex18 completed

---
+++ 01_Easy/16. Binary Search.js
@@ -0,0 +1,32 @@
+// Iterative Solution -- Time Complexity = O(log n) -- Space Complexity = O(1)
+function binarySearch(array, target) {
+  let leftPtr = array[0];
+  let rightPtr = array.length-1;
+
+  // Once left is greater than right we've reached the end
+  while (leftPtr <= rightPtr) {
+    let midPtr = Math.floor(((leftPtr + rightPtr) / 2));
+    let midPoint = array[midPtr];
+    
+    if (midPoint == target) {
+      return midPtr; // return index of target
+    } else if (midPoint < target) {
+      // Search right
+      leftPtr = midPtr+1;
+    } else if (midPoint > target) {
+      // Search left
+      rightPtr = midPtr-1;
+    } 
+  }
+  
+  return -1;
+}
+
+
+/*
+ Cases
+ - even length array
+ - odd length array
+ 
+
+*/
---
+++ 01_Easy/18. Bubble Sort.js
@@ -0,0 +1,24 @@
+function bubbleSort(array) {
+  let swapped = true;
+
+  while (swapped) {
+    swapped = false;
+
+    for (let i = 0; i < array.length-1; i++) {
+      let cur = array[i];
+      let nxt = array[i+1];
+      
+      if (cur > nxt) {
+        array[i+1] = array[i];
+        array[i] = nxt;
+        swapped = true;
+      }
+    }
+  }
+  return array;
+}
+
+
+/* NOTES 
+
+*/
