13d28831faf2a4ac02c66bec48a929a337b14de5
Edward Blanciak <blanciak.edward2@gmail.com>
2022-08-12 23:50:07 -0400

    ex13 complete: optimal time and space complexity

---
+++ 01_Easy/13. Remove Duplicates From Linked List.js
@@ -0,0 +1,45 @@
+// Time Complexity - O(n) 
+// Space  Complexity - O(1)
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  let curPtr = linkedList;
+  let nextPtr = curPtr.next;
+  
+  while (nextPtr != null) {
+    let temp = nextPtr.next
+    
+    if (curPtr.value === nextPtr.value) { // duplicate -- skip it and keep checking
+      if (nextPtr.next === null) {
+        curPtr.next = null;
+      }
+      nextPtr = temp;
+    } else if (curPtr.value !== nextPtr.value) { // no duplicate -- break link, shift curPtr to nextPtr pos. and shift nextPtr 1 right
+      curPtr.next = nextPtr; // break the link
+      curPtr = nextPtr;
+      nextPtr = temp; // shift 1 right
+    }
+  }
+
+  
+  // print(linkedList)
+  return linkedList;
+}
+
+function print(linkedList) {
+  let cur = linkedList;
+  
+  while (cur.next != null) {
+    let temp = cur;
+    console.log(cur)
+    cur = temp.next;
+  }  
+}
+
+
+
