6d5a6d491468fd4c75f611a68e8078f47fb5fb7e
Edward Blanciak <blanciak.edward2@gmail.com>
2022-08-13 00:09:48 -0400

    updated comments and renamed variables

--- 01_Easy/4. Tournament Winner.js
+++ 01_Easy/4. Tournament Winner.js
@@ -1,36 +1,42 @@
 function tournamentWinner(competitions, results) {
-  // Scoreboard Creation
-    // Flattens the nested arrays and removes duplicates 
-  let teams = [...new Set(competitions.flat())]      // O(n)?
-  let scores = teams.reduce((a,v) => (               // O(n)?
+              // Scoreboard Creation //
+  
+  // Flattens the nested arrays and removes duplicates 
+  let teams = [...new Set(competitions.flat())] // O(n)?
+  
+  // Populate map with all teams
+  let scores = teams.reduce((a,v) => ( // O(n)?
     { ...a, [v]: 0}), {}
   )
 
-  let counter = 0;
 
-  // Calculate Points
-  results.forEach(item => {                          // O(n)
-    if (item === 0) { // 0 - away team one (position 1)
-      scores[competitions[counter][1]] += 3;
-    } else { // 1 - home team won (position 0)
-      scores[competitions[counter][0]] += 3;
+              // Calculate Points //
+  
+  let game = 0;
+  
+  results.forEach(gameWinner => { // O(n)
+    if (gameWinner === 0) { // 0 - away team won 
+      scores[competitions[game][1]] += 3; // add 3 pts to scores["away team name"] -- we are accessing competitions to grab the name
+    } else { // 1 - home team won 
+      scores[competitions[game][0]] += 3; // add 3 pts to scores["home team name"]
     }
-    counter++;
+    game++;
   })
 
-  // Determine Winner
+              // Determine Winner // 
+  
   let winner;
   let winningPts = 0;
   
-  for (key in scores) {                              // O(n)
+  for (key in scores) { // O(n)
     if (scores[key] >= winningPts) {
       winningPts = scores[key]
       winner = key;
     }
   }
 
-  console.log(scores)  
-  console.log(winner,"-", winningPts)  
+  // console.log(scores)  
+  // console.log(winner,"-", winningPts)  
   
   return winner;
 }
