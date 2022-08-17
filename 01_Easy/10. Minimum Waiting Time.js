fe81d14a491af316bd3be1c0299d8c6d951c2548
Edward Blanciak <blanciak.edward2@gmail.com>
2022-08-16 23:58:36 -0400

    completed in optimal time and space complexity. needs comments updated

---
+++ 01_Easy/10. Minimum Waiting Time.js
@@ -0,0 +1,32 @@
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
+function minimumWaitingTime(queries) {
+  // If there is 1 query, there is no wait time (first query is executed immediately)
+  if (queries.length === 1) {
+    return 0;
+  }
+  
+  // Sort Queries
+  queries.sort((a,b) => a-b) // O(nlogn)
+
+  let minWaitingTime = queries[0];
+  let prevSum = queries[0];
+  
+  for (let i = 2; i < queries.length; i++) { // O(n)
+    prevSum += queries[i-1];
+    minWaitingTime += prevSum;
+  }
+  
+  return minWaitingTime;
+}
+
+
+/*
+- Sort Array O(nlogn)
+[3, 2, 1, 2, 6]
+[1, 2, 2, 3, 6]
+
+- loop starting with index 1
+    - keep running total 
+
+*/
