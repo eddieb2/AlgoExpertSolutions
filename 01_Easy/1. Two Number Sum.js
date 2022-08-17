64127a92bb5a7c3b6df5f4edaa6bf7005741cdbf
Edward Blanciak <blanciak.edward2@gmail.com>
2022-08-17 12:56:12 -0400

    optimal time and space complexity. comments updated.

--- 01_Easy/1. Two Number Sum.js
+++ 01_Easy/1. Two Number Sum.js
@@ -1,5 +1,6 @@
-// Time Complexity: O(n^2)
-// Space Complexity: O(n)
+// Solution 1: Brute Force //
+// Time Complexity: O(n^2) //
+// Space Complexity: O(n) //
 
 function twoNumberSum(array, targetSum) {
   
@@ -21,4 +22,45 @@ function twoNumberSum(array, targetSum) {
 	return targets;
 }
 
+
+// Solution 2: Optimal Solution		//
+// Time Complexity: O(n)					//
+// Space Complexity: O(n)					//
+function twoNumberSum(array, targetSum) {
+  let hash = {};
+    
+  for (let i = 0; i < array.length; i++) { // O(n)
+    let complement = targetSum - array[i];
+    
+    if (!(array[i] in hash)) { // O(1)
+      hash[array[i]] = i;
+    }
+
+    if (complement in hash) { // O(1)
+      if (hash[complement] !== i) {
+        return [complement, array[i]]
+      }  
+    }
+    
+  }
+
+  return [];
+}
+
+/*
+- Add all numbers to the hash with corresponding indicies:
+				-> array = [4, 6, 1]
+				-> hash  = { 
+											4: 0, 
+										  6: 1,
+                      1: 2 
+                    }
+                    
+- Check if the complement is in the hash:
+				-> i = 0 , array[0] = 4, comp = 1
+        	->  "1" in hash -> true
+          	-> i !== hash[complement (1)] -> 0 != 2 -> true
+          		-> return [1,4]
+        
+*/ 
 
